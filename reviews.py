af50fa901933824e7046cf5df66661831339441e
Clare E <136653618+clareegan@users.noreply.github.com>
2023-09-08 00:36:38 +0000

    comitting changes

--- reviews.py
+++ reviews.py
@@ -1,7 +1,3 @@
-# add your code here
-
-# add imports to script
-
 import pandas as pd
 import zipfile
 
@@ -12,27 +8,12 @@ import zipfile
 
 df = pd.read_csv('data/winemag-data-130k-v2.csv.zip', index_col=0)
 
-wine_df = df
-wine_df['count'] = 1
+reviews_per_country = df.country.value_counts()
+
+review_points = df.groupby('country')['points'].mean().round(1)
+
+reviews_merged = pd.DataFrame.merge(reviews_per_country, review_points, on='country', how='inner')
+
+reviews_merged.to_csv('data/reviews-per-country.csv')
 
 
-
-review_av = df.groupby('country').agg({'count': 'sum', 'points': 'mean'}).reset_index()
-review_av['points'] = review_av['points'].round(1)
-sorted_reviews = review_av.sort_values(by='count', ascending=False)
-
-sorted_reviews.to_csv('data/reviews-per-country.csv', index=False)
-
-
-
-# reviews = df.loc[:, ['country', 'count', 'points']]
-
-# column_list = df.columns.tolist()
-
-
-#print(df.groupby(['country']).points.agg(['mean']))
-
-
-df['Count'] = df.country.value_counts()
-
-print(review_av) 
